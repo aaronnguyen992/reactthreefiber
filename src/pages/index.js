ed03fd095a999e052599cc2ac5d1593794739ae9
Aaron Nguyen <aaronnguyen992@gmail.com>
2020-01-20 12:48:50 -0800

    added static folder and updated 3d import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,49 @@
-import React, { useState, useRef } from "react"
-import { Canvas, useFrame } from 'react-three-fiber';
+import React, { useState, useRef, useEffect } from "react";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Canvas, extend, useThree, useFrame } from 'react-three-fiber';
 import { useSpring, a } from 'react-spring/three';
 
 import './style.css';
+
+extend({ OrbitControls })
+
+const Space = () => {
+  const[model, setModel] = useState()
+  useEffect(() => {
+    new GLTFLoader().load('/scene.gltf', setModel)
+  })
+  
+  return model ? <primitive object={model.scene} /> : null
+}
+
+const Controls = () => {
+  const orbitRef = useRef()
+  const { camera, gl } = useThree()
+
+  useFrame(() => {
+    orbitRef.current.update()
+  })
+  return (
+    <orbitControls 
+      args={[camera, gl.domElement]}
+      ref={orbitRef}
+      autoRotate
+      maxPolarAngle={Math.PI / 3}
+      minPolarAngle={Math.PI / 3}
+    />
+  )
+}
+
+const Plane = () => {
+  return(
+    <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
+      <planeBufferGeometry attach="geometry" args={[100, 100]}/>
+      <meshPhysicalMaterial attach="material" color="gray"/>
+    </mesh>
+  )
+}
 
 const Box = () => {
   // const meshRef = useRef();
@@ -10,8 +51,9 @@ const Box = () => {
   const [active, setActive] = useState(false);
   const props = useSpring({
     scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
-    color: hovered ? "gray" : "hotpink"
+    color: hovered ? "hotpink" : "gray"
   })
+
   // useFrame(() => {
   //   meshRef.current.rotation.y += 0.01
   // })
@@ -23,16 +65,26 @@ const Box = () => {
       onPointerOut={() => setHovered(false)}
       onClick={() => setActive(!active)}
       scale={props.scale}
+      castShadow
     >
+      <ambientLight />
+      <spotLight position={[0, 5, 10]} penumbra={1} castShadow/>
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]}/>
-      <a.meshBasicMaterial attach="material" color={props.color}/>
+      <a.meshPhysicalMaterial attach="material" color={props.color}/>
     </a.mesh>
   )
 }
 
 const IndexPage = () => (
-  <Canvas>
+  <Canvas camera={{ position: [0, 0, 5] }} onCreated={({ gl }) => {
+    gl.shadowMap.enabled = true
+    gl.shadowMap.type = THREE.PCFSoftShadowMap
+  }}>
+    <fog attach="fog" args={["white", 5, 10]} />
+    <Controls />
     <Box />
+    <Plane />
+    <Space />
   </Canvas>
 )
 
